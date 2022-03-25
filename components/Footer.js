efc23dad5d5e268ddf7ae31c6217f7f83dedc6b1
mcclintsayshi <manakanejustin@gmail.com>
2022-03-26 00:58:35 +0200

    updated the links and made solid bg

--- components/Footer.js
+++ components/Footer.js
@@ -2,16 +2,31 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-300 dark:bg-gray-900 dark:text-gray-100">
-      <div className="container mx-auto py-6">
-        Powered by
-        <a
-          href="https://www.themoviedb.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          The Movie Database
-        </a>
+    <footer className="bg-black text-gray-100">
+      <div className="container mx-auto py-6 flex justify-between">
+        <p>
+          Powered by&nbsp;
+          <a
+            className="underline underline-offset-2"
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The Movie Database
+          </a>
+        </p>
+
+        <p>
+          made by&nbsp;
+          <a
+            className="underline underline-offset-2"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/clintsayshello"
+          >
+            Clinton
+          </a>
+        </p>
       </div>
     </footer>
   );
