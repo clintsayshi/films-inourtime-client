c046dec4db4072d380df772c7a0561dafb2329b0
mcclintsayshi <manakanejustin@gmail.com>
2022-03-25 13:38:44 +0200

    updated header

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,93 @@
 import React from "react";
+import Link from "next/link";
 
 function Header() {
-  return (
-    <header classNme="dark:bg-gray-900">
-      <nav classNme="container mx-auto flex justify-between">
-        <div classNme="flex gap-4">
-          <a classNme="text-lg dark:text-gray-100" href="#">
-            Home
-          </a>
+  const toggleDarkMode = () => {
+    console.log(
+      "HEREEEEE",
+      document.documentElement.getAttribute("data-theme")
+    );
 
-          <div classNme="hidden sm:flex gap-4">
-            <a classNme="text-lg dark:text-gray-100">Movies</a>
-            <a classNme="text-lg dark:text-gray-100">Series</a>
+    if (
+      document.documentElement.getAttribute("data-theme") == "light" ||
+      document.documentElement.getAttribute("data-theme") == null
+    ) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <header className="bg-gray-50 dark:bg-gray-900">
+      <nav className="container mx-auto px-4 py-2 flex items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <button className="sm:hidden text-gray-900 dark:text-gray-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="1.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 8h16M4 16h16"
+              />
+            </svg>
+          </button>
+
+          <Link href="/">
+            <a className="text-lg font-medium dark:text-gray-100">Home</a>
+          </Link>
+
+          <div className="hidden sm:flex gap-4">
+            <a className="text-lg font-medium dark:text-gray-100">Movies</a>
+            <a className="text-lg font-medium dark:text-gray-100">Series</a>
           </div>
         </div>
 
-        <button classNme="dark:text-gray-100">Dark Mode</button>
+        <div className="flex items-center space-x-2">
+          <button
+            onClick={toggleDarkMode}
+            className="text-gray-900 dark:text-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="1.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            </svg>
+          </button>
+          <button className="text-gray-900 dark:text-gray-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="1.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </nav>
     </header>
   );
