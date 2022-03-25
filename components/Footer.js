d13664e74df239882d9612523b3b858d74f45297
mcclintsayshi <manakanejustin@gmail.com>
2022-03-25 13:40:45 +0200

    added dark theme to footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="dark:bg-gray-900">
-      <div className="container mx-auto min-h-32">
+    <footer className="bg-gray-300 dark:bg-gray-900 dark:text-gray-100">
+      <div className="container mx-auto py-6">
         Powered by
         <a
           href="https://www.themoviedb.org/"
