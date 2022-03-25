795ebed0b2cce75e4c01f6c2a7d43a7b9e6c6ccd
mcclintsayshi <manakanejustin@gmail.com>
2022-03-25 13:39:07 +0200

    created and updated MovieCard few times,

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -8,34 +8,39 @@ function MovieCard({ movie, mediaConfig }) {
 
   return (
     <Link key={movie.id} href={`/${id}`}>
-      <a className="relative block rounded-2xl group">
-        <div className="absolute inset-0 bg-black bg-opacity-30 z-30 group-hover:bg-opacity-0 transition-opacity ease-in-out duration-300"></div>
-        <div className="relative z-10 w-full h-96">
+      <a className="relative block hover:shadow py-4 hover:px-4 transition-all ease-in-out duration-300 group">
+        <div className="relative w-full h-96 lg:h-96 mb-2">
           <Image
-            className="block rounded-0 object-cover object-top"
+            className="block object-cover object-top"
             src={`${secure_base_url}/t/p/${poster_sizes[4]}/${poster_path}`}
             layout="fill"
             alt=""
           />
         </div>
-        <div className="absolute bottom-4 left-4 z-40">
-          <h3 className="text-lg sm:text-2xl font-bold text-white">
-            {original_title}
-          </h3>
-          <p className="text-lg text-white">{release_date.substring(0, 4)}</p>
-          <div className="flex items-center gap-2">
-            <div className="relative h-6 w-10">
-              <Image
-                className="object-cover"
-                src="https://m.media-amazon.com/images/G/01/IMDb/BG_rectangle._CB1509060989_SY230_SX307_AL_.png"
-                layout="fill"
-                alt=""
+        <h3 className="text-lg underline underline-offset-4 dark:text-gray-100">
+          {original_title}&nbsp;({release_date.substring(0, 4)})
+        </h3>
+        <div className="flex items-center justify-between gap-2">
+          <small className="text-gray-900 dark:text-gray-100">
+            {vote_average} rating
+          </small>
+
+          <button className="hidden dark:text-gray-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               />
-            </div>
-            <small className="text-white text-base font-medium">
-              {vote_average} rating
-            </small>
-          </div>
+            </svg>
+          </button>
         </div>
       </a>
     </Link>
