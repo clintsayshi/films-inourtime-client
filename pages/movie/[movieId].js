28b3cc1022755b80631b9eb61512edfda32a3bf8
mcclintsayshi <manakanejustin@gmail.com>
2022-04-05 02:59:27 +0200

    updated movie page with new layout and replacing image with trailer

--- pages/movie/[movieId].js
+++ pages/movie/[movieId].js
@@ -4,6 +4,9 @@ import Image from "next/image";
 import Layout from "../../components/Layout";
 import client from "../../utils.js/apollo-client";
 import { GET_MOVIE, GET_TRENDING_MOVIES } from "../../utils.js/queries";
+import Link from "next/link";
+import YoutubeEmbed from "../../components/YoutubeEmbed";
+import { monthNames } from "../../utils.js/dates";
 
 function Movie({ data }) {
   const {
@@ -12,6 +15,7 @@ function Movie({ data }) {
     vote_average,
     release_date,
     backdrop_path,
+    videos,
     status,
     production_companies,
     genres,
@@ -22,50 +26,85 @@ function Movie({ data }) {
 
   const [posterModal, setPosterModal] = useState(false);
 
+  const getTrailer = () => {
+    const trailer = videos.find((x) => x.type === "Trailer");
+    return trailer.key;
+  };
+
+  const getDate = (date) => {
+    const year = new Date(date).getFullYear();
+    const day = date.substring(8);
+    const month = monthNames[new Date(date).getMonth()];
+
+    return `${day} ${month} ${year}`;
+  };
+
   return (
     <Layout>
-      <div
-        className={`relative w-full h-96 bg-[${secure_base_url}${poster_sizes[5]}/${poster_path}]`}
-      >
-        <Image
-          className="block object-cover object-center position-fixed"
-          src={`${secure_base_url}/original/${backdrop_path}`}
-          alt=""
-          layout="fill"
-          priority
-        />
+      <div className="container mx-auto p-4 space-y-10">
+        <header className="space-y-6">
+          <small className="flex items-center space-x-2 text-base text-gray-800 sm:text-sm dark:text-gray-100">
+            <span title="The release date">{getDate(release_date)}</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span title="The original title">{original_title}</span>
+          </small>
+          <h1 className="text-4xl sm:text-5xl font-display font-bold dark:text-gray-100">
+            {title}
+          </h1>
+        </header>
+
+        <div></div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 space-y-4 md:space-y-0 md:space-x-12">
+          <div
+            className={`relative w-full bg-[${secure_base_url}${poster_sizes[5]}/${poster_path}]`}
+          >
+            <YoutubeEmbed videoId={getTrailer()} />
+          </div>
+
+          <div className="space-y-2">
+            <div className="flex justify-between items-center">
+              <small className="text-gray-900 dark:text-gray-100">
+                {vote_average}&nbsp;IMDb rating
+              </small>
+
+              <button className="hidden">to the MOON</button>
+            </div>
+
+            <div className="py-2 flex items-center gap-2">
+              {genres.map((genre) => (
+                <Link
+                  key={genre.id}
+                  href={{
+                    pathname: "/genre",
+                    query: { genreId: genre.id, name: genre.name },
+                  }}
+                >
+                  <a className="w-max block px-3 py-1 text-sm text-center text-gray-900 dark:text-gray-100 border rounded-full">
+                    {genre.name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+
+            <p className="text-gray-900 dark:text-gray-100">{overview}</p>
+          </div>
+        </div>
       </div>
 
       <section className="container mx-auto ">
-        <header className="p-4 flex flex-col items-center gap-2">
-          <h2 className="text-2xl text-center uppercase font-medium dark:text-gray-100">
-            {title} - {release_date.substring(0, 4)}
-          </h2>
-          <small
-            title="Original title of the movie"
-            className="text-sm dark:text-gray-400"
-          >
-            {original_title}
-          </small>
-          <small className="text-gray-900 dark:text-gray-100">
-            {vote_average}&nbsp;IMDb rating
-          </small>
-          <div className="py-2 flex items-center gap-2">
-            {genres.map((genre) => (
-              <a
-                key={genre.id}
-                className="w-max block px-4 py-1 text-sm text-center text-gray-900 dark:text-gray-100 border rounded-full"
-                href="#"
-              >
-                {genre.name}
-              </a>
-            ))}
-          </div>
-        </header>
-
-        <div className="container mx-auto p-4 flex flex-col">
-          <p className="text-gray-900 dark:text-gray-100">{overview}</p>
-        </div>
+        <div className="container mx-auto p-4 flex flex-col"></div>
       </section>
     </Layout>
   );
